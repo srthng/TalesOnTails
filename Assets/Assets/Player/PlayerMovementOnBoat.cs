e54420e0754be9f0f38737090e7e6901f8c20542
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 13:24:39 -0300

    a

--- Assets/Assets/Player/PlayerMovementOnBoat.cs
+++ Assets/Assets/Player/PlayerMovementOnBoat.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 
 public class PlayerMovementOnBoat : MonoBehaviour
 {
-    
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private float speed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
 
 }
