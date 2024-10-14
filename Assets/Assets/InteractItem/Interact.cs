b1b396eef558a4ec4c56d64e06abf35e4b083883
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-14 11:27:25 -0300

    a

--- Assets/Assets/InteractItem/Interact.cs
+++ Assets/Assets/InteractItem/Interact.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
+    private Transform playerTransform;
+    public GameObject InteractObject;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        InteractObject = GameObject.Find("Interact");
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player")){
-            Destroy(other.gameObject);
+            if (Input.GetKey(KeyCode.E))
+            {
+                InteractObject.gameObject.SetActive(false);
+            }
         }
     }
 }
