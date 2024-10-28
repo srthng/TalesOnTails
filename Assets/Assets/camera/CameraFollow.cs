71a4de03568c3a3abb95f74cee268e0b390673fa
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-28 08:42:04 -0300

    a

--- Assets/Assets/camera/CameraFollow.cs
+++ Assets/Assets/camera/CameraFollow.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
+    public GameObject CameraLimit; 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        if (player.position.y != CameraLimit.transform.position.y)
+        {
+            Vector3 desiredPosition = player.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+        else if (player.GetComponent<Collider2D>)
+        {
+            Debug.Log("a");
+        }
     }
 }
 
