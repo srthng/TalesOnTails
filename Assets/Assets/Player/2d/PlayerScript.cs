b18950bd257def35fd422f8b713de8c212248947
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-30 13:31:13 -0300

    a

--- Assets/Assets/Player/2d/PlayerScript.cs
+++ Assets/Assets/Player/2d/PlayerScript.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    private UIDocument document;
+    private VisualElement HealthBar;
+    private VisualElement StaminaBar;
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
-    private int PlayerLife;
+    private int PlayerLife = 100;
     private bool isFacingRight = true;
     public float radius;
     public LayerMask enemies;
     public Animator anim;
+
+    private void Start()
+    {
+        document = GetComponent<UIDocument>();
+        HealthBar = document.rootVisualElement.Q<VisualElement>("FillHealth");
+        StaminaBar = document.rootVisualElement.Q<VisualElement>("FillStamina");
+    }
 
     private void Update()
     {
@@ -49,7 +61,11 @@ public class PlayerScript : MonoBehaviour
             anim.SetBool("IsWalking", false);
         }
 
-
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("IsAttacking", true);
+        }
+        
 
     }
     private void FixedUpdate()
